cc6cca9d158e504db289be78bd5763a5527ff9cc
developer1 <abiodunbolarinw44@gmail.com>
2023-06-07 22:49:22 +0300

    New Banner Completed

--- src/Components/banner/Banner.jsx
+++ src/Components/banner/Banner.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./banner.css";
 const Banner=() => {
     return(
-        <div className="banner-container" >
-            <div className="banner-inner">
-            <div className="banner-right">
-
-                </div>
+        <div className='banner-container'>
+            <div className="banner-img">
+                <h1 className='banner-text-lg'>Shopping Without Limit</h1>
+                <h2 className='banner-text-sm'>Choose the best option for you, and it does not matter whether you are.</h2>
+                <button className='banner-left-button'>Shop Now</button>
             </div>
         </div>
     )
     
     }
     export default Banner;
+
+    
